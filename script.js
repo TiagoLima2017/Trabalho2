eea8e10c3d6c871f8aa26a92feeb7e7a896f9030
Tiago Lima <tiago_lima213@hotmail.com>
2021-12-06 21:56:26 +0000

    Trabalho adicionado

---
+++ script.js
@@ -0,0 +1,37 @@
+var binary = document.getElementById("bin");
+var octal = document.getElementById("oct")
+var decimal = document.getElementById("dec");
+var hexadecimal = document.getElementById("hex");
+var inputEl = document.getElementsByClassName("output");
+var calcEl = document.getElementsByClassName("outpute");
+var igualEl = document.getElementById("igual");
+var resulte = 0;
+var mapper = {
+    "bin":2,
+    "oct":8,
+    "dec":10,
+    "hex":16,
+};
+for (var i = 0; i < inputEl.length; i++) {
+    var element = inputEl[i];  
+      
+    element.addEventListener("keyup", e => {
+        var inputId = e.target.id;
+        var inputValue = parseInt(document.getElementById(inputId).value, mapper[inputId]);
+        var result = Number.isNaN(inputValue) ? "Inválido" : inputValue;
+
+        if ("bin" != inputId) { binary.value = result.toString(mapper["bin"]) }
+        if ("oct" != inputId) { octal.value = result.toString(mapper["oct"]) }
+        if ("dec" != inputId) { decimal.value = result.toString(mapper["dec"]) }
+        if ("hex" != inputId) { hexadecimal.value = result.toString(mapper["hex"]) }
+    });
+};
+igualEl.addEventListener("click", e => {
+    var calculo = document.getElementById("calc");
+    if (resulte == calculo.value) return;
+ 
+    resulte = Number(calculo.value);
+
+      binary.value = resulte.toString(mapper["bin"]);
+      decimal.value = Number(resulte).toString(mapper["dec"]); 
+});
